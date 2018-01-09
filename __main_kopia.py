12b7e33225c95ca22804bb123b6ef52d4d21555b
Marek Malbrandt <marek.malbrandt@gmail.com>
2018-01-09 21:53:31 +0100

    Dodanie kolorów, dźwięków, naprawa algorytmu.

--- __main_kopia.py
+++ __main_kopia.py
@@ -1,4 +1,14 @@
+'''
+Author: Marek Malbrandt <marek.malbrandt@gmail.com>
+'''
+
 from time import sleep
+from termcolor import colored
+
+LICZBA_GRACZY = 5
+
+MUZYCZKA_RUNDA = 'runda-gong.wav'
+MUZYCZKA_PUNKT = 'score.wav'
 
 punkty = 0
 
@@ -29,7 +39,7 @@ def runda(lista_graczy: dict):
     return rzuty
 
 
-def sprawdz_czy_bylo_wszystko(rzuty:dict):
+def sprawdz_czy_bylo_wszystko(rzuty: dict):
     byly = []
 
     for gracz, rzut in rzuty.items():
@@ -43,11 +53,18 @@ def sprawdz_czy_bylo_wszystko(rzuty:dict):
     return False
 
 
+def pusc_muzyczke(param):
+    import winsound
+
+    winsound.PlaySound(param, winsound.SND_ASYNC)
+
+
 def policz_punkty(rzuty: dict):
     punkty = {}
     lista_u = []
 
     for wartosc in rzuty.values():
+
         if wartosc not in lista_u:
             lista_u.append(wartosc)
 
@@ -55,24 +72,31 @@ def policz_punkty(rzuty: dict):
         return {}
 
     for gracz1, rzut1 in rzuty.items():
+        czy_puscic = False
         punkty[gracz1] = 0
 
         if rzut1 is RZUT_KAMIEN:
             for gracz2, rzut2 in rzuty.items():
                 if gracz2 is not gracz1 and rzut2 is not RZUT_PAPIER:
                     punkty[gracz1] += 1
+                    czy_puscic = True
 
         if rzut1 is RZUT_PAPIER:
             for gracz2, rzut2 in rzuty.items():
                 if gracz2 is not gracz1 and rzut2 is not RZUT_NOZYCE:
                     punkty[gracz1] += 1
+                    czy_puscic = True
+
 
         if rzut1 is RZUT_NOZYCE:
             for gracz2, rzut2 in rzuty.items():
                 if gracz2 is not gracz1 and rzut2 is not RZUT_KAMIEN:
                     punkty[gracz1] += 1
+                    czy_puscic = True
 
-    punkty_znormalizowane = {}
+        if czy_puscic:
+            pusc_muzyczke(MUZYCZKA_PUNKT)
+
 
     max_punkty = policz_gracz_max_pkt(punkty)
 
@@ -94,13 +118,11 @@ def policz_punkty(rzuty: dict):
         if pkt is max_punkty:
             wygrani[gracz] = 1
 
-    wszystko = False
-
     wszystkie_kombinacje = sprawdz_czy_bylo_wszystko(rzuty)
 
     # sprawdzamy czy nie ma remisu lub wszystkich kombinacji
     if len(wygrani) is len(rzuty) or wszystkie_kombinacje:
-        wygrani = {'wszystkie kombinacje'}
+        wygrani = {}
 
     return wygrani
 
@@ -117,54 +139,111 @@ def policz_gracz_max_pkt(punkty):
 
     return max_punkty
 
-def gra():
-    gracze = ['Marek', 'Kamil', "Paweł",  'Sylwia']
+
+def wyswietl_graczy(wygrani:dict, z_punktami:bool = False):
+    for imie, wynik in wygrani.items():
+        do_wyswietlenia = "\t" + imie
+
+        if z_punktami:
+            do_wyswietlenia += ': ' + str(wynik)
+
+        print(colored(do_wyswietlenia, 'white', attrs=['concealed']))
+
+
+def kli_kla_klo(opoznienie):
+    print('Kli...')
+    sleep(opoznienie * 2)
+    print('Kla...')
+    sleep(opoznienie * 2)
+    print('Klo!')
+
+def gra(gracze, do_ilu = 3, przerwa_sekundy = 2, opoznienie = 0.250):
 
     lista_graczy = {}
     wyniki = {}
+    nr_rundy = 1
+
+    # dodajemy graczy
     for gracz in gracze:
         dodaj_gracza(lista_graczy, gracz)
         wyniki[gracz] = 0
 
     # dopoki ktos nie wygral
     gramy = True
+    wygrana = False
     while gramy:
-        for gracz, wynik in wyniki.items():
-            if wynik > 2:
+        pusc_muzyczke('runda-gong.wav')
+        print('----------------------------')
+        print(colored(('Runda nr: '+str(nr_rundy)), color='white', attrs=['dark', 'bold']))
+        nr_rundy += 1
+        rzuty = runda(lista_graczy)
 
-                for gracz, punkty in wyniki.items():
-                    if punkty >= 3:
-                        print('\tWygrany: ', gracz,' [̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]')
-                return
+        kli_kla_klo(opoznienie)
 
-            rzuty = runda(lista_graczy)
-            print("Gracze wylosowali:")
-            for gracz, rzut in rzuty.items():
-                print(gracz + ': ' + RZUTY[rzut])
+        # wyświetlamy rzuty graczy
+        print(colored("Gracze wylosowali:", 'green'))
+        sleep(opoznienie)
+        wyswietl_rzuty(rzuty)
 
-            print('------')
-            print("Po punkcie zyskuja:")
-            wygrani = policz_punkty(rzuty)
-            if len(wygrani) is 0:
-                print('remis lub wszystkie kombinacje')
-            else:
-                print(wygrani)
-            try:
-                for gracz, pkt in wygrani.items():
-                    if gracz in wyniki:
-                        wyniki[gracz] += 1
-            except:
-                pass
+        sleep(opoznienie)
+        print(colored("Po punkcie zyskuja:", 'blue'))
+        wygrani = policz_punkty(rzuty)
+        sleep(opoznienie)
+        if len(wygrani) is 0:
+            print(colored("\tremis lub wszystkie kombinacje", 'red'))
+        else:
+            if len(wygrani) > 0:
+                wyswietl_graczy(wygrani)
+        try:
+            wyniki = policz_wyniki(wygrani, wyniki)
+        except:
+            pass
 
-            print(wyniki)
-            print('----------------')
-            sleep(2)
+        print(colored('Obecna punktacja:', 'cyan'))
+        wyswietl_graczy(wyniki, True)
+
+        for imie, wynik in wyniki.items():
+            if wynik >= 3:
+                sleep(opoznienie)
+                pusc_muzyczke('ta-da.wav')
+                print(colored('***** WYGRANA *****', 'red'))
+                wyswietl_wygranych(wyniki, do_ilu)
+                gramy = False
+                break
+
+        sleep(przerwa_sekundy)
 
         if not gramy:
-
             break
+
+
+def policz_wyniki(wygrani, wyniki):
+    for gracz, pkt in wygrani.items():
+        if gracz in wyniki:
+            wyniki[gracz] += 1
+
+    return wyniki
+
+
+def wyswietl_rzuty(rzuty):
+    for gracz, rzut in rzuty.items():
+        print("\t", colored(gracz + ': ' + RZUTY[rzut], 'white'))
+
+
+def wyswietl_wygranych(wyniki, wygrywa_pkt = 3):
+    for gracz, punkty in wyniki.items():
+        if punkty >= wygrywa_pkt:
+            print('\tWygrany: ', colored(gracz, 'magenta'), colored(' [̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]', 'green'))
+
 
 if __name__ == "__main__":
     # obsługa klawiatury
     from msvcrt import getch
-    gra()
+    gracz_prefix = 'gracz_'
+    gracze = []
+
+    # for i in range(0, LICZBA_GRACZY):
+    #     gracze.append('gracz_' + str(i))
+
+    gracze = ['Marek', 'Paweł', 'Sylwia']
+    gra(gracze)
